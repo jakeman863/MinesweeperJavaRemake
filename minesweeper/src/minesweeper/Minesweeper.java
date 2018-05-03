7edf56c6c3cf6273cb0343966194aaa4477d89e1
jakeman863 <jaketheman01@gmail.com>
2018-05-02 23:59:17 -0500

    Minesweeper Remake 1.0
    A remake of the classic Microsoft game Minesweeper, developed with a two (2) man team from scratch for a group project in my Object-Oriented Programming class.

---
+++ minesweeper/src/minesweeper/Minesweeper.java
@@ -0,0 +1,201 @@
+
+package minesweeper;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+
+
+public class Minesweeper extends Application
+{
+    // n = number of tiles horizontal
+    // m = number of tiles vertical
+    private int n;
+    private int m;
+
+    // width and height of the application
+    double appH = 1024;
+    double appW = 600;
+
+    // width and height of the game tiles
+    int gameH = 32;
+    int gameW = 32;
+
+    char[][] mineField;
+
+    BorderPane layout;
+
+    InitiateBoard board;
+
+    int mines;
+
+    Tile[][] killmenow;
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        Menu mBar1 = new Menu("Game");
+        MenuItem beginner = new MenuItem("Beginner");
+        MenuItem intermediate = new MenuItem("Intermediate");
+        MenuItem expert = new MenuItem("Expert");
+        Menu mBar2 = new Menu("Help");
+        MenuItem instructions = new MenuItem("Instructions");
+        MenuItem about = new MenuItem("About");
+
+        MenuBar menuBar = new MenuBar();
+
+        mBar1.getItems().add(beginner);
+        mBar1.getItems().add(intermediate);
+        mBar1.getItems().add(expert);
+
+        mBar2.getItems().add(instructions);
+        mBar2.getItems().add(about);
+
+        menuBar.getMenus().addAll(mBar1,mBar2);
+
+        layout = new BorderPane();
+        layout.setTop(menuBar);
+        
+        Text scoreBar = new Text("");
+
+        Group grid = new Group();
+
+        beginner.setOnAction(new EventHandler<ActionEvent>()
+        {
+          public void handle(ActionEvent t)
+          {
+            board = new InitiateBoard(1);
+            mineField = board.getBoard();
+            n = board.getWidth();
+            m = board.getHeight();
+            mines = board.getMines();
+            killmenow = new Tile[n][m];
+            updateGrid(grid, board, scoreBar);
+          }
+        });
+
+        intermediate.setOnAction(new EventHandler<ActionEvent>()
+        {
+          public void handle(ActionEvent t)
+          {
+            board = new InitiateBoard(2);
+            mineField = board.getBoard();
+            n = board.getWidth();
+            m = board.getHeight();
+            mines = board.getMines();
+            killmenow = new Tile[n][m];
+            updateGrid(grid, board, scoreBar);
+
+          }
+        });
+
+        expert.setOnAction(new EventHandler<ActionEvent>()
+        {
+          public void handle(ActionEvent t)
+          {
+            board = new InitiateBoard(3);
+            mineField = board.getBoard();
+            n = board.getWidth();
+            m = board.getHeight();
+            mines = board.getMines();
+            killmenow = new Tile[n][m];
+            updateGrid(grid, board, scoreBar);
+
+          }
+        });
+
+        instructions.setOnAction(new EventHandler<ActionEvent>()
+        {
+          public void handle(ActionEvent t)
+          {
+            BorderPane newPane = new BorderPane();
+       
+            Text Rules = new Text("You are presented with a board of squares. Some squares contain mines (bombs), others don't. If you click on a square containing a bomb, you lose.\n If you manage to click all the squares (without clicking on any bombs) you win.\n" +
+"Clicking a square which doesn't have a bomb reveals the number of neighbouring squares containing bombs. Use this information plus some guess work to avoid the bombs.\n" +
+"To open a square, point at the square and click on it. To mark a square you think is a bomb, point and right-click.");
+            Rules.setTextAlignment(TextAlignment.CENTER);
+            newPane.setCenter(Rules);
+            Stage stage = new Stage();
+            stage.setTitle("Instructions");
+            stage.setScene(new Scene(newPane, appH, appW));
+            stage.show();
+          }
+        });
+        
+        about.setOnAction(new EventHandler<ActionEvent>()
+        {
+          public void handle(ActionEvent t)
+          {
+            BorderPane newPane = new BorderPane();
+       
+            Text About = new Text("CREATED BY: \n\n Jake Manning\n + \n Jonathan Stewart");
+            About.setTextAlignment(TextAlignment.CENTER);
+            newPane.setCenter(About);
+            Stage stage = new Stage();
+            stage.setTitle("About");
+            stage.setScene(new Scene(newPane, 450, 450));
+            stage.show();
+          }
+        });
+
+        layout.setCenter(grid);
+        // layout = new BorderPane();
+        //layout = new BorderPane();
+        layout.setBottom(scoreBar);
+        //layout.setCenter();
+
+        // StackPane root = new StackPane();
+        Scene scene = new Scene(layout, appH, appW, Color.GREY);
+
+        primaryStage.setTitle("Minesweeper");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    private void updateGrid(Group grid, InitiateBoard board, Text scoreBar)
+    {
+      grid.getChildren().clear();
+
+      String[][] strArr = new String[mineField.length][mineField[0].length];
+      for (int i = 0; i < mineField.length; i++)
+      {
+          for (int j = 0; j < mineField[0].length; j++)
+          {
+              strArr[i][j] = Character.toString(mineField[i][j]);
+          }
+      }
+
+      for( int i=0; i < n; i++) {
+          for( int j=0; j < m; j++) {
+
+              // create node
+              // Tile tile = new Tile( "0", i * gridWidth, j * gridHeight, 50, 50
+              Tile tile = new Tile(strArr[i][j], i * gameW, j * gameH, gameW, gameH, grid, board, scoreBar);
+              killmenow[i][j] = tile;
+              // add node to group
+              grid.getChildren().add(tile);
+          }
+      }
+
+      for( int i=0; i < n; i++) {
+          for( int j=0; j < m; j++) {
+
+              killmenow[i][j].setArray(killmenow);
+
+          }
+      }
+
+    }
+}
---
+++ minesweeper/src/minesweeper/Tile.java
@@ -0,0 +1,183 @@
+package minesweeper;
+
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.*;
+
+
+public class Tile extends StackPane
+{
+        public static int totalBombs;
+        public static int totalTiles;
+        public static int flagCounter;
+        public static int openedCounter;
+        
+        Rectangle rectangle;
+        Label label;
+        boolean isBomb = false;
+        boolean opened = false;
+        boolean flagged = false;
+        
+        int x;
+        int y;
+
+        int val;
+
+        Tile killmenow[][];
+
+        InitiateBoard board;
+
+        Group grid;
+
+        public Tile( String name, int x, int y, double width, double height, Group grid, InitiateBoard board, Text scoreBar)
+        {
+            this.x = x;
+            this.y = y;
+            this.board = board;
+            //val = Integer.parseInt(name);
+            this.grid = grid;
+            
+            this.flagCounter = board.mines;
+            this.openedCounter = 0;
+            this.totalBombs = board.mines;
+            this.totalTiles = board.xTiles * board.yTiles;
+            
+            scoreBar.setText("Flag Counter: " + flagCounter);
+
+            // create rectangle
+            rectangle = new Rectangle(width, height);
+            rectangle.setStroke(Color.GREY);
+            rectangle.setFill(Color.BLACK);
+
+            // create label
+            label = new Label(name);
+            label.setVisible(false);
+
+            // detect if rectangle is a bomb
+            if(name.contains("*"))
+            {
+              isBomb = true;
+            }
+
+            //Button button = new Button();
+
+            //button.setPrefWidth(width);
+            //button.setPrefHeight(height);
+
+            // set position
+            setTranslateX(x);
+            setTranslateY(y);
+
+            this.setOnMousePressed((MouseEvent event) -> {
+                MouseButton button = event.getButton();
+                if(button==MouseButton.PRIMARY){
+                    open(1, scoreBar);
+                }else if(button==MouseButton.SECONDARY){
+                    open(2, scoreBar);
+                }
+            });
+        
+        //(e->open());
+            
+            // rectangle.setFill(Color.LIGHTGREY);
+            // label.setVisible(true););
+
+        //     setOnAction(new EventHandler<ActionEvent>() {
+        //     @Override
+        //     public void handle(ActionEvent event) {
+        //         rectangle.setFill(Color.LIGHTGREY);
+        //         label.setVisible(true);
+        //
+        //     }
+        // });
+
+            getChildren().addAll(rectangle, label);
+        }
+
+        
+        
+        public void open(int clickNum, Text scoreBar)
+        {
+          if(opened)
+          {
+            return;
+          }
+          else if(clickNum == 2 && !flagged)
+          {
+              rectangle.setFill(Color.BLUE);
+              flagged = true;
+              flagCounter -= 1;
+              scoreBar.setText("Flag Counter: " + flagCounter);
+          }
+          else if(clickNum == 2 && flagged)
+          {
+              rectangle.setFill(Color.BLACK);
+              flagged = false;
+              flagCounter += 1;
+              scoreBar.setText("Flag Counter: " + flagCounter);
+          }
+          else if(isBomb && clickNum == 1)
+          {
+            grid.getChildren().clear();
+            label = new Label("Game Over - YOU LOSE!");
+            grid.getChildren().addAll(label);
+            scoreBar.setText("");
+          }
+          else if(label.getText().contains("0") && clickNum == 1)
+          {
+            rectangle.setFill(Color.LIGHTGREY);
+            opened = true;
+            openedCounter += 1;
+            List<Tile> adjacent;
+            adjacent = openEmpty(this);
+
+            for(int i = 0; i < adjacent.size(); i++)
+            {
+              adjacent.get(i).open(1, scoreBar);
+            }
+          }
+          else if(clickNum == 1)
+          {
+            opened = true;
+            openedCounter += 1;
+            rectangle.setFill(Color.LIGHTGREY);
+            label.setVisible(true);
+          }
+          if(totalTiles - openedCounter == totalBombs)
+          {
+            grid.getChildren().clear();
+            label = new Label("Game Over - YOU WIN!");
+            grid.getChildren().addAll(label);
+            scoreBar.setText("");
+          }
+        }
+
+        private List<Tile> openEmpty(Tile tile)
+        {
+            List<Tile> adjacent = new ArrayList<>();
+
+            if ((tile.x / 32) - 1 >= 0 && (tile.y / 32) + 1 < board.yTiles)              {adjacent.add(killmenow[(tile.x / 32) - 1][(tile.y / 32) + 1]);} // O
+            if ((tile.x / 32) - 1 >= 0)                                                  {adjacent.add(killmenow[(tile.x / 32) - 1][(tile.y / 32)]);}     // O
+            if ((tile.x / 32) - 1 >= 0 && (tile.y / 32) - 1 >= 0 )                       {adjacent.add(killmenow[(tile.x / 32) - 1][(tile.y / 32) - 1]);}
+            if ((tile.y / 32) - 1 >= 0 )                                                 {adjacent.add(killmenow[(tile.x / 32)][(tile.y / 32) - 1]);}
+            if ((tile.x / 32) + 1 < board.xTiles && (tile.y / 32) - 1 >= 0)              {adjacent.add(killmenow[(tile.x / 32) + 1][(tile.y / 32) - 1]);}
+            if ((tile.x / 32) + 1 < board.xTiles)                                        {adjacent.add(killmenow[(tile.x / 32) + 1][(tile.y / 32)]);}
+            if ((tile.x / 32) + 1 < board.xTiles && (tile.y / 32) + 1 < board.yTiles)    {adjacent.add(killmenow[(tile.x / 32) + 1][(tile.y / 32) + 1]);}
+            if ((tile.y / 32) + 1 < board.yTiles)                                        {adjacent.add(killmenow[(tile.x / 32)][(tile.y / 32) + 1]);}
+
+            return adjacent;
+        }
+
+        public void setArray(Tile[][] killmenow)
+        {
+          this.killmenow = killmenow;
+        }
+
+}
